4e1622688bfe6617006be20f1bc9f523724ab158
Gabriel Perez <gabrielrperez.dev@gmail.com>
2023-12-25 05:17:18 -0500

    add changing player functionality

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const score1El = document.querySelector("#score--1");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
 
+//Players
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
+
 //Score
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Dice element
 const diceEL = document.querySelector(".dice");
@@ -27,6 +33,13 @@ btnRoll.addEventListener("click", function () {
   diceEL.src = `dice-${dice} (1).png`;
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
